9d8e4bf9839f4b88949bf8f5890039ff2109958c
rafaelminski <rafael.minski57@gmail.com>
2023-03-28 20:32:39 -0300

    terceiro commit

--- Horas1.java
+++ Horas1.java
@@ -5,7 +5,7 @@ public class Horas1 {
      public static void main(String[] args) {
         //variáveis
         Scanner entrada;
-        int hora, minuto, segundo, tempo,tempoh;
+        int hora, minuto, segundo, tempo,tempoh, tempom, tempos;
         
         //entradas
         entrada = new Scanner(System.in);
@@ -23,10 +23,16 @@ public class Horas1 {
         tempo = tempo + minuto * 60;
         tempo = tempo + segundo;
         tempoh = tempo/3600;
+        tempom = tempo%3600;
+        tempom = tempom/60;
+        tempos = tempom%60;
+
 
         //saída
         System.out.printf("Se passaram %d segundos desde 00:00 \n", tempo);
         System.out.printf("Se passaram %d horas desde 00:00 \n", tempoh);
+        System.out.printf("Se passaram %d minutos desde 00:00 \n", tempom);
+        System.out.printf("Se passaram %d segundos desde 00:00 \n", tempos);
         
 
 
