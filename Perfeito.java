ed84fd4f6471aacbccddbdd29efbc8ac0f65149f
rafaelminski <rafael.minski57@gmail.com>
2023-06-06 20:45:09 -0300

    aula 6 de junho

--- Perfeito.java
+++ Perfeito.java
@@ -4,24 +4,27 @@ public class Perfeito {
     public static void main(String[] args){
         //variaveis
         Scanner entrada;
-        int x, n_div, i, soma;
+        int x, n_div, i, soma, acm;
         //entradas
         entrada = new Scanner(System.in);
         System.out.println("Entre com um valor");
         x = entrada.nextInt();
         entrada.close();
         //processamento
-       
+        soma= 0;
+        acm=0;
         for(i=1;i<x;i++){
             if(x%i == 0 ){
-                n_div = i;
-                soma = n_div + n_div; 
-                if(soma==x){
-                    System.out.println("É perfeito");
-                }else{
-                    System.out.println("Não é perfeito");
-                }
+                acm=acm+i;
+                //n_div = i;
+                //soma = n_div + n_div; 
+                
             }
+        }
+        if(acm==x){
+            System.out.println("É perfeito");
+        }else{
+            System.out.println("Não é perfeito");
         }
 }
 }
