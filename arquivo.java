a2dd9349c0cae2f0847884e14157ed1939ee52d4
rafaelminski <rafael.minski57@gmail.com>
2023-09-05 23:35:16 -0300

    arquivos

--- arquivo.java
+++ arquivo.java
@@ -6,9 +6,13 @@ public class arquivo {
     public static void main(String[] args) {
 
          int i;
+         String nome = "rafael";
+         String nome_arquivo;
+
+         nome_arquivo = String.format("arquivo_%s.txt", nome);
 
         try{
-        PrintWriter saida_pares = new PrintWriter("pares.txt");
+        PrintWriter saida_pares = new PrintWriter(nome_arquivo);
         PrintWriter saida_impares = new PrintWriter("impares.txt");
         for(i = 0;i<=10;i++){
             if(i%2 ==0){
